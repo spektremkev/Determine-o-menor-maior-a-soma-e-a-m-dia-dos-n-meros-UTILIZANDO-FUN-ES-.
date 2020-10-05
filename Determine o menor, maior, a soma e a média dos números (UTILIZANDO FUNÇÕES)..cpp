9f49cbd895da82b036693bb4b443cc061a8c1d03
Gabriel Dos S N Martins <50385858+spektremkev@users.noreply.github.com>
2020-10-04 22:07:10 -0300

    Add files via upload

---
+++ Determine o menor, maior, a soma e a média dos números (UTILIZANDO FUNÇÕES)..cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*atividade 2_questão 6
+
+int n1, n2, n3, igual;
+
+	int igual(){ 
+			if (n1==n2==n3)
+			 printf ("Você possui tres numeros iguais");
+				else 
+					printf ("Você possui tres numeros iderentes "); 
+					
+						else if(n1  n2 && n2 > n3) 
+						printf ("O numero %d e o e o menor  ",n2);
+				 	
+						else if(n1 < n2 && n1 < n3) 
+							printf ("O numero %d e o maior ",n1); 
+							
+								else if(n2 > n3) 
+									printf ("O numero %d e o maior ",n3); 
+									
+									else if(n1 > n2)
+										
+										printf ("O numero %d e o maior ",n2); 
+											}
+
+int main(){
+	printf("Digite um numero que de deseja: ");
+		 scanf( "%d", &n1, &n2, &n3 );
+
+		printf(iqual());
+	
+	
+}*/
+int main(){
+
+int a=0, b=0, x=0;
+
+            a = 3;
+
+            if (a < 3)
+
+                        b = a * 3;
+
+            else if (a != 3)
+
+                        b = a + a;
+
+            a = a * 5 - b;
+
+            x = (b<5) ? a - 7: b + a;
+            printf("%d",x);
+
+}
